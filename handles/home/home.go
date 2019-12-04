93a90efa2e507abb7534113db698f40181d5d77e
GerryLuo <dkluohanga@163.com>
2019-12-04 17:49:12 +0800

    图片再换一把

--- handles/home/home.go
+++ handles/home/home.go
@@ -16,10 +16,10 @@ const (
 var (
 	testTags = []string{"男孩", "女孩", "可爱", "唯美"}
 	testImgs = [][]string{
-		[]string{"https://b-ssl.duitang.com/uploads/item/201709/22/20170922162149_snyk3.jpeg", "https://img3.duitang.com/uploads/item/201602/18/20160218003123_aLMyv.jpeg", "https://img.zcool.cn/community/019e235c0a3f2da801209252c0497f.jpg@1280w_1l_2o_100sh.jpg"},
-		[]string{"https://b-ssl.duitang.com/uploads/item/201809/26/20180926162125_vjbwi.jpg", "https://b-ssl.duitang.com/uploads/item/201607/25/20160725102949_2earM.jpeg", "https://img.zcool.cn/community/01045058a578d6a801219c77f4a434.jpg"},
-		[]string{"https://hbimg.huabanimg.com/f6ee1b095215b8c2955fd07e56e2739b2671cd3579f0d-5feCNB_fw658", "https://img5.imgtn.bdimg.com/it/u=2717062052,3164034025&fm=26&gp=0.jpg", "https://cdn.duitang.com/uploads/item/201602/23/20160223124339_d2NkX.jpeg"},
-		[]string{"https://b-ssl.duitang.com/uploads/item/201509/04/20150904014041_Lw8Cv.jpeg", "https://b-ssl.duitang.com/uploads/item/201602/10/20160210211239_JCnsw.jpeg", "https://cdn.duitang.com/uploads/item/201410/26/20141026191422_yEKyd.thumb.700_0.jpeg"},
+		[]string{"https://pic4.zhimg.com/50/v2-6f3d900004a053b38736d70a95e1ff4d_hd.jpg", "https://pic1.zhimg.com/80/v2-e127049dbe074821174cfd16954ac00d_hd.jpg", "https://img.zcool.cn/community/019e235c0a3f2da801209252c0497f.jpg@1280w_1l_2o_100sh.jpg"},
+		[]string{"https://pic1.zhimg.com/80/v2-79dde9e02680f1594b159d3b00058d6f_hd.jpg", "https://pic4.zhimg.com/80/v2-516691a7fc4f536f5587146b36376314_hd.jpg", "https://img.zcool.cn/community/01045058a578d6a801219c77f4a434.jpg"},
+		[]string{"https://pic1.zhimg.com/80/v2-5dee6f4a28ba3252e03c2d2268eae93f_hd.jpg", "https://pic2.zhimg.com/80/v2-176bb323efbf92d0e139d43cc38590ff_hd.jpg", "https://pic1.zhimg.com/80/v2-47534989141c9418e6a7992ebcbed3e9_hd.jpg"},
+		[]string{"https://pic1.zhimg.com/80/v2-b4eebad17f6637f4c231ab34dae45473_hd.jpg", "https://pic4.zhimg.com/80/v2-3d78919445f889a0bc8903ed40abb100_hd.jpg", "https://pic4.zhimg.com/80/v2-63ed0eaf5efd35ae2aa400ea4f2c2b17_hd.jpg"},
 	}
 )
 
